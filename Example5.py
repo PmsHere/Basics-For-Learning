1a0f6045b7bc21a785f951747b7ff520b701937d
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-05 15:33:30 +0530

    .lower function

---
+++ Example5.py
@@ -0,0 +1,24 @@
+answer = input("Do you want to hear a joke? ")
+
+affirmative_responses = ["yes", "y"]
+negative_responses = ["no", "n"]
+
+if answer.lower() in affirmative_responses:
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedburg (RIP)
+elif answer.lower() in negative_responses:
+    print("Fine.")
+else:
+    print("I don't understand.")
+    
+    #method-2
+    
+    answer = input("Do you want to hear a joke? ")
+
+if answer.lower() in affirmative_responses:
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedburg (RIP)
+elif answer.lower() in negative_responses:
+    print("Fine.")
+else:
+    print("I don't understand.")
