7cbb63feec9290426a2f0e0659d5c24df69a10a7
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-05 15:20:04 +0530

    or

---
+++ Example3.py
@@ -0,0 +1,9 @@
+answer = input("Do you want to hear a joke? ")
+
+if answer == "Yes" or answer == "yes":
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedberg (RIP)
+elif answer == "No" or answer == "no" :
+    print("Fine.")
+else:
+    print("I don't understand.")
