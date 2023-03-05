09ee681f9c8995247731813391d2677b92d12f40
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-05 15:18:36 +0530

    Create Example2.py
    If , elif , else

---
+++ Example2.py
@@ -0,0 +1,9 @@
+answer = input("Do you want to hear a joke? ")
+
+if answer == "Yes":
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedberg (RIP)
+elif answer == "No":
+    print("Fine.")
+else:
+    print("I don't understand.")
