c1aca1f3bbfb491f76ac8769359d2293a93c4c08
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-06 10:19:03 +0530

    % modulo usage

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,3 +6,37 @@
 # Tip: % (modulo) tells you what's left over when you divide one number by another
 # ex. number % 3 == 0
 
+#step1
+
+for number in range(1,101):
+    print(number)
+    
+#step2
+
+for number in range(1,101):
+    if number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
+        
+  #step3
+  
+  for number in range(1,101):
+    if number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+        
+  #step4
+  
+  for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
