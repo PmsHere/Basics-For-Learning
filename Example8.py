f85cc6fe30fa86a5ce99c538ad0af9db0e4cb7ac
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-06 09:57:36 +0530

    range

---
+++ Example8.py
@@ -0,0 +1,7 @@
+# Challenge: Print out the square of the numbers 1 to 10
+for number in range(1,2,3,4,5,6,7,8,9,10):
+    print(number, "squared is", number * number)
+    
+ # Challenge: Print out the square of the numbers 1 to 10
+for number in range(1,10):
+    print(number, "squared is", number * number)
