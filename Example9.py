989a62a449e4a6bf2b59186150909782dba5ea4a
Shalu Alexander Lukose <72794063+PmsHere@users.noreply.github.com>
2023-03-06 10:05:20 +0530

    len , type function

---
+++ Example9.py
@@ -0,0 +1,8 @@
+# here's how you access elements of a list
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+first_animal = animals[0]
+print(first_animal)
+third_animal = animals[2]
+print(third_animal)
+
+print("They're are this many things:", len(random_things))
